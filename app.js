d477b5dfee13aeb0668ba4a73718d427c97c29de
Gabriel <gehughes28@gmail.com>
2023-11-01 13:09:45 +0100

    small changes, button function for contact form, changed breakpoint from 780px wide to 800px wide

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+//making observer
 const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
         console.log(entry)
@@ -9,6 +10,15 @@ const observer = new IntersectionObserver((entries) => {
     });
 });
 
-
+//selecting all hidden elements
 const hiddenElements = document.querySelectorAll('.hidden');
+//observing hidden elements
 hiddenElements.forEach((el) => observer.observe(el));
+
+
+function sent() {
+    let contactName = document.getElementById('name-input').value;
+    console.log(contactName)
+    alert(`Thank you for getting in touch with us ${contactName}.\nWe will get back to you as soon as possible.`)
+}
+
